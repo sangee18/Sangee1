f2553fa40791a7221c5218b6c4f4227c3daea773
sangee18 <52812563+sangee18@users.noreply.github.com>
2019-07-12 11:25:43 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<0)
+    printf("Invalid");
+    else if(x%2!=0)
+    printf("Odd");
+    else
+    printf("Even");
+return 0;
+}
